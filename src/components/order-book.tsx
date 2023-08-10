8685d5a77162f5e2892cdc7478d271c79cf019fc
jlee-3 <leejhnthn93@gmail.com>
2023-08-10 10:48:30 +0800

    feat: bids display and updates

--- src/components/order-book.tsx
+++ src/components/order-book.tsx
@@ -1,10 +1,13 @@
 import example from "../utils/example.json";
 import { useState, useEffect } from "react";
+import _ from "lodash";
 
 type LastPriceStatus = "gain" | "loss" | "same";
 
 export default function OrderBook() {
-  // const [bids, setBids] = useState<any[]>([]);
+  const [bidPrice, setBidPrice] = useState<number[]>([]);
+  const [bidSize, setBidSize] = useState<number[]>([]);
+  const [bidTotals, setBidTotals] = useState<any>([]);
   const [lastPrice, setLastPrice] = useState<number>();
   const [lastPriceStatus, setLastPriceStatus] = useState<LastPriceStatus>();
 
@@ -12,7 +15,19 @@ export default function OrderBook() {
   const data = response?.data;
   const rawBids = data.bids.slice(0, 8); // sell
 
+  const orderbookSocket = new WebSocket("wss://ws.btse.com/ws/oss/futures");
   const lastPriceSocket = new WebSocket("wss://ws.btse.com/ws/futures");
+
+  const subscribeOrderbook = () => {
+    if (orderbookSocket.readyState === orderbookSocket.OPEN) {
+      const payload = {
+        op: "subscribe",
+        args: [`update:BTCPFC_0`],
+      };
+      console.log("subscribing to orderbook api: " + JSON.stringify(payload));
+      orderbookSocket.send(JSON.stringify(payload));
+    }
+  };
 
   const subscribeLastPrice = () => {
     if (lastPriceSocket.readyState === lastPriceSocket.OPEN) {
@@ -25,8 +40,104 @@ export default function OrderBook() {
     }
   };
 
+  const unsubscribe = () => {
+    if (orderbookSocket.readyState === orderbookSocket.OPEN) {
+      const payload = {
+        op: "unsubscribe",
+        args: [`update:BTCPFC_0`],
+      };
+      console.log(
+        "un-subscribing to oss delta for incremental update: " +
+          JSON.stringify(payload)
+      );
+      orderbookSocket.send(JSON.stringify(payload));
+    }
+  };
+
+  orderbookSocket.onopen = () => {
+    subscribeOrderbook();
+  };
+
   lastPriceSocket.onopen = () => {
     subscribeLastPrice();
+  };
+
+  orderbookSocket.onmessage = (e) => {
+    if (typeof e.data === "string") {
+      // console.log("received: '" + e.data + "'");
+      const raw = JSON.parse(e.data);
+      // const topic = raw && raw.topic ? raw.topic : "";
+      const rawBids = raw?.data?.bids;
+
+      updateBids(rawBids, raw?.data?.type);
+    }
+  };
+
+  const updateBids = (quotes: any[], type: string) => {
+      console.log("resetting with quotes: ", quotes.slice(0, 8));
+      setBidPrice(
+        quotes.slice(0, 8).map((quote) => {
+          return Number(quote[0]);
+        })
+      );
+      updateBidSize(
+        quotes.slice(0, 8).map((quote) => {
+          return Number(quote[1]);
+        })
+      );
+    } else if (quotes?.[0] && bidPrice?.[0] && type === "delta") {
+      let tempBidPrice = bidPrice;
+      let tempBidSize = bidSize;
+
+      for (let i = 0; i < quotes.length; i++) {
+        // append highest price to start
+        if (quotes[i][1] !== "0" && Number(quotes[i][0]) > bidPrice[0]) {
+          tempBidPrice.pop();
+          tempBidSize.unshift(Number(quotes[i][1]));
+          tempBidSize.pop();
+        } else if (
+          quotes[i][1] !== "0" &&
+          Number(quotes[i][0]) <= bidPrice[0] &&
+          Number(quotes[i][0]) >= bidPrice[7]
+        ) {
+          // replace old bid
+          if (
+            bidPrice.includes(Number(quotes[i][0])) &&
+            !bidSize.includes(Number(quotes[i][1]))
+          ) {
+            tempBidPrice.splice(
+              bidPrice.indexOf(Number(quotes[i][0])),
+              1,
+              Number(quotes[i][0])
+            );
+            tempBidSize.splice(
+              bidSize.indexOf(Number(quotes[i][1])),
+              1,
+              Number(quotes[i][1])
+            );
+            // insert new bid
+          } else if (!bidPrice.includes(Number(quotes[i][0]))) {
+            let index = _.sortedIndex(
+              tempBidPrice.reverse(),
+              Number(quotes[i][0])
+            );
+
+            tempBidPrice.splice(index, 0, Number(quotes[i][0]));
+            tempBidPrice.reverse().pop();
+            tempBidSize.reverse().splice(index, 0, Number(quotes[i][1]));
+            tempBidSize.reverse().pop();
+          }
+        }
+
+        setBidPrice(tempBidPrice);
+        updateBidSize(tempBidSize);
+      }
+    }
+  };
+
+  const updateBidSize = (bidSize: number[]) => {
+    setBidSize(bidSize);
+    updateTotalSize();
   };
 
   lastPriceSocket.onmessage = (e) => {
@@ -52,15 +163,17 @@ export default function OrderBook() {
   //   setBids([...bids, response.data.bids.slice(0, 7)]);
   // }, [response, bids]);
 
-  const appendTotals = (data: any) => {
+  const updateTotalSize = () => {
+    const totals = [];
     let cumulativeTotal = 0;
-    for (let i = data.length - 1; i >= 0; i--) {
-      cumulativeTotal = cumulativeTotal + Number(data[i][1]);
-      data[i] = [...data[i], cumulativeTotal];
-    }
-  };
 
-  appendTotals(rawBids);
+    for (let i = bidSize.length - 1; i >= 0; i--) {
+      cumulativeTotal = cumulativeTotal + Number(bidSize[i]);
+      totals.push(cumulativeTotal);
+    }
+
+    setBidTotals(totals.reverse());
+  };
 
   const formatNumber = (number: any, digits: number) => {
     return Number(number).toLocaleString(undefined, {
@@ -80,18 +193,25 @@ export default function OrderBook() {
         </div>
         <p className="text-text-head">Total</p>
       </div>
-      <div>
-        {rawBids.map((bid) => {
-          return (
-            <div key={bid[0]} className="px-2 flex justify-between">
-              <div className="flex w-[50%] justify-between">
-                <div className="text-text-sell">{formatNumber(bid[0], 1)}</div>
-                <div>{formatNumber(bid[1], 0)}</div>
+      <div className="pb-2">
+        {bidPrice &&
+          bidSize &&
+          bidPrice.map((bid: number, index: number) => {
+            return (
+              <div
+                key={bid}
+                className="px-2 flex justify-between hover:bg-background-hover"
+              >
+                <div className="flex w-[50%] justify-between">
+                  <div className="text-text-sell">
+                    {formatNumber(bidPrice[index], 1)}
+                  </div>
+                  <div>{formatNumber(bidSize[index], 0)}</div>
+                </div>
+                <div className="">{formatNumber(bidTotals[index], 0)}</div>
               </div>
-              <div className="">{formatNumber(bid[2], 0)}</div>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
       <div
         className={`py-2 flex justify-center font-semibold items-center ${
@@ -132,7 +252,6 @@ export default function OrderBook() {
           </svg>
         )}
       </div>
-      <div>Status: {lastPriceStatus}</div>
     </div>
   );
 }
